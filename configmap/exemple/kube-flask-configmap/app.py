be6d39698dff2e3b2e1a8a09f21f6a3d2324dbc4
fuekeng <fuekengbilly@gmail.com>
2026-02-13 15:41:19 +0100

    update

--- configmap/exemple/kube-flask-configmap/app.py
+++ configmap/exemple/kube-flask-configmap/app.py
@@ -1,12 +1,29 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-with open("/config/FLASK_PORT") as f:
-    PORT = int(f.read().strip())
+# Lire depuis variable d'environnement avec valeur par défaut
+PORT = int(os.environ.get("FLASK_PORT", 5001))
+
 @app.route("/")
 def hello():
     return f"Hello from Flask (read from file) on port {PORT}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
+
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# with open("/config/FLASK_PORT") as f:
+#     PORT = int(f.read().strip())
+# @app.route("/")
+# def hello():
+#     return f"Hello from Flask (read from file) on port {PORT}"
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=PORT)

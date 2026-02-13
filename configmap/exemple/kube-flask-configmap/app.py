b31e7abd3bf853762b42e9e89be75d19e80601be
fuekeng <fuekengbilly@gmail.com>
2026-02-13 15:58:07 +0100

    change config map port number

--- configmap/exemple/kube-flask-configmap/app.py
+++ configmap/exemple/kube-flask-configmap/app.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 
 # Lire depuis variable d'environnement avec valeur par défaut
-PORT = int(os.environ.get("FLASK_PORT", 5001))
+PORT = int(os.environ.get("FLASK_PORT", 5000))
 
 @app.route("/")
 def hello():
@@ -12,18 +12,3 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
-
-
-
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# with open("/config/FLASK_PORT") as f:
-#     PORT = int(f.read().strip())
-# @app.route("/")
-# def hello():
-#     return f"Hello from Flask (read from file) on port {PORT}"
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=PORT)

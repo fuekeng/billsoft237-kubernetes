a00b099d26d25dceddb745ebb0f0ed46aa0cfae5
fuekeng <fuekengbilly@gmail.com>
2026-02-13 17:31:05 +0100

    using config map with volume

--- configmap/exemple/kube-flask-configmap/app.py
+++ configmap/exemple/kube-flask-configmap/app.py
@@ -1,15 +1,32 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
-# Lire depuis variable d'environnement avec valeur par défaut
-PORT = int(os.environ.get("FLASK_PORT", 5000))
-print(f"Flask will run on port: {PORT}")
-
+with open("/config/FLASK_PORT") as f:
+    PORT = int(f.read().strip())
 @app.route("/")
 def hello():
     return f"Hello from Flask (read from file) on port {PORT}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
+
+
+
+
+
+#from flask import Flask
+# import os
+
+# app = Flask(__name__)
+
+# # Lire depuis variable d'environnement avec valeur par défaut
+# PORT = int(os.environ.get("FLASK_PORT", 5000))
+# print(f"Flask will run on port: {PORT}")
+
+# @app.route("/")
+# def hello():
+#     return f"Hello from Flask (read from file) on port {PORT}"
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=PORT)
